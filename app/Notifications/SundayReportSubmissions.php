f1364d77be5aa8889027944e3f6ba0e39dd82a3c
KWangechi <rachel.karanja@strathmore.edu>
2023-07-26 00:11:12 +0300

    Committing a missed file

---
+++ app/Notifications/SundayReportSubmissions.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SundayReportSubmissions extends Notification
+{
+    use Queueable;
+
+    // public $message;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Sunday Report Submission')
+            // ->mailer(auth()->user()->email)
+            ->line('This is to notify you that I have submitted my Sunday Report with all the comments from the service.')
+            ->salutation('Thank you. Kind regards');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
