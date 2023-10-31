bf94df9eeb59b00c195260c979bf030baaefe015
KWangechi <rachel.karanja@strathmore.edu>
2023-10-31 23:32:29 +0300

    Finished with the edit page for the user leaves and made the UI to be dark version. Now done with leave functionality for the user

--- resources/views/user/leave/edit.blade.php
+++ resources/views/user/leave/edit.blade.php
@@ -1,16 +1,15 @@
-<x-layout bodyClass="g-sidenav-show  bg-gray-200">
+<x-layout bodyClass="g-sidenav-show  bg-gray-200 dark-version">
     <x-navbars.sidebar activePage="leaves"></x-navbars.sidebar>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
 
         <!-- Navbar -->
-        <x-navbars.navigation titlePage="Leaves"></x-navbars.navigation>
-
+        <x-navbars.navigation titlePage="Edit Leaves"></x-navbars.navigation>
         <div class="container">
-            <br>
-            <h1 class="text-center">
+            <h3 class="text-center">
                 <strong>
-                Edit Leave Request
-                    </strong> </h1>
+                    Edit Leave Request
+                </strong>
+            </h3>
 
             <!-- Display error or success message -->
             @if (session('success_message'))
@@ -25,56 +24,48 @@
                 <a class="btn-close" data-bs-dismiss="alert" aria-label="Close"></a>
             </div>
             @endif
-                        <div class="leave_request_form">
-                            <form action="{{ route('user.leave.update', [auth()->user()->id, $leave->id]) }}" method="POST" id="createLeaveForm" enctype="multipart/form-data">
-                                @csrf
-                                @method('PATCH')
-
-                                <!-- Reason -->
-                                <div class="mt-4">
-                                    <x-label for="reason" :value="__('Reason')" />
-                                    <select name="reason">
-                                        <option value="{{$leave->reason}}">{{$leave->reason}}</option>
-                                        <option value="">Select the reason</option>
-                                        <option value="Sickness">Sickness</option>
-                                        <option value="Bereavement">Bereavement</option>
-                                        <option value="Travelling">Travelling</option>
-                                        <option value="Personal Reasons">Personal Reasons(Prefer not to say)</option>
-                                    </select>
-                                </div>
-
-                                <!-- Start Date -->
-                                <div class="mt-4">
-                                    <x-label for="start_date" :value="__('Start Date')" />
-
-                                    <x-input id="start_date" class="block mt-1 w-full" type="date" name="start_date" value="{{$leave->start_date}}" required>
-
-                                    </x-input>
-                                </div>
-
-                                <!-- error message for start date and end date -->
-                                <!-- @if (session('leave_error_message'))
-                                <div class="input">
-                                    {{ session('leave_error_message') }}
-                                </div>
-                                @else
-                                @endif -->
-
-                                <!-- End Date -->
-                                <div class="mt-4">
-                                    <x-label for="end_date" :value="__('End Date')" />
-
-                                    <x-input id="end_date" class="block mt-1 w-full" type="date" name="end_date" value="{{$leave->end_date}}" required />
-                                </div>
-                                <br>
-                                <x-button class="ml-4" id="createLeaveRequest" type="submit">
-                                    {{ __('Update') }}
-                                </x-button>
-                            </form>
+            <div class="leave_request_form">
+                <form action="{{ route('user.leave.update', [auth()->id(), $leave->id]) }}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <div class="row mt-4">
+                        <div class="col-md-6">
+                            <label class="form-label font-weight-bold">Reason</label>
+                            <div class="input-group input-group-outline my-1">
+                                <select class="form-select-md form-control" name="reason" id="reason">
+                                    <option value="{{$leave->reason}}" selected disabled>{{$leave->reason}}</option>
+                                    <option value="Bereavement">Bereavement</option>
+                                    <option value="Sickness">Sickness</option>
+                                    <option value="Personal Reasons">Personal Reasons</option>
+                                    <option value="Temporary Absence">Temporary Absence</option>
+                                    <option value="Travelling">Travelling</option>
+                                </select>
+                            </div>
                         </div>
-                        <div class="modal-footer">
-                            <a class="btn btn-secondary" href="{{ route('user.leaves.index', auth()->user()->id) }}">Cancel</a>
+
+                        <div class="col-md-6">
+                            <label class="form-label font-weight-bold">Start Date</label>
+                            <div class="input-group input-group-outline my-1 ">
+                                <input type="date" name="start_date" class="form-control" value="{{$leave->start_date}}">
+                            </div>
                         </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <label class="form-label">End Date</label>
+                            <div class="input-group input-group-outline my-2">
+                                <input type="date" name="end_date" class="form-control" value="{{$leave->end_date}}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-md bg-gradient-info w-10 mt-4 mb-3">Save</button>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-secondary" href="{{ route('user.leaves.index', auth()->user()->id) }}">Cancel</a>
+            </div>
         </div>
     </main>
 </x-layout>
